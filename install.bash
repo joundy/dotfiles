af694a1ac9d755e50ee77166753a21f9cbf1ec77
joundy <hafizjoundys@gmail.com>
2024-03-30 20:52:29 +0700

    update: add dunstrc configs

--- install.bash
+++ install.bash
@@ -9,4 +9,5 @@ sudo ln -s $HOME/dotfiles/.config/gtk-3.0 $HOME/.config/
 sudo ln -s $HOME/dotfiles/.config/rofi $HOME/.config/
 sudo ln -s $HOME/dotfiles/.config/sway $HOME/.config/
 sudo ln -s $HOME/dotfiles/.config/waybar $HOME/.config/
+sudo ln -s $HOME/dotfiles/.config/dunst $HOME/.config/
 sudo ln -s $HOME/dotfiles/.config/nvim/lua/user $HOME/.config/nvim/lua/
